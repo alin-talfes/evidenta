770407e8dc888df424be7ccd5ae0b50492623c3f
Alin Talfeș <alin.talfes@icloud.com>
2026-09-16 21:32:57 +0300

    refactor(runtime): remove legacy observer shim

--- js/pedepse-legacy-observer-kill.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  'use strict';
-
-  // Neutralizează controllerele vechi care instalau observatori largi pe DOM.
-  // Noile controllere folosesc inițializare deterministă, fără MutationObserver pe body/main.
-  window.__EVIDENTA_PEDEPSE_OPTIONAL_FIX__ = true;
-  window.__EVIDENTA_PEDEPSE_MODES_V4__ = true;
-  window.__EVIDENTA_DISCLOSURE_HARDENING__ = true;
-})();
